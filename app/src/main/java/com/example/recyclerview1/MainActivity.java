6f5d6d6e54a58c08cdb6e7c2e50ddb613a2d9ac4
siddhihandal <siddhinh17.extc@coep.ac.in>
2021-08-09 19:56:09 +0530

    images alligned

--- app/src/main/java/com/example/recyclerview1/MainActivity.java
+++ app/src/main/java/com/example/recyclerview1/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.pic6, R.drawable.pic7, R.drawable.pic8,
                 R.drawable.pic9, R.drawable.pic10, R.drawable.pic14, R.drawable.pic15};
 
-        String[] name = {"pic1","pic2","pic12","pic6","pic7","pic8","pic9","pic10", "pic14", "pic15"};
+        String[] name = {"pic1","pic2","pic3","pic4","pic5","pic6","pic7","pic8", "pic9", "pic10"};
 
         mainModels = new ArrayList<>();
         for(int i=0;i<pics.length;i++){
@@ -45,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
         mainAdapter = new MainAdapter(MainActivity.this , mainModels);
         recyclerView.setAdapter(mainAdapter);
+
+
 
     }
 }
